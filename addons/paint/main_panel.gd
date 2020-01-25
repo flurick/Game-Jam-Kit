8b6f918d78e84128d38f6a8e699675975cb269b6
flurick <the3drick@gmail.com>
2020-01-25 18:23:02 +0100

    paint: Resizable canvas and focus tweak

--- addons/paint/main_panel.gd
+++ addons/paint/main_panel.gd
@@ -5,6 +5,7 @@ extends Panel
 func _ready():
 #	$Toolbox/HBoxContainer/Tool.value = 8
 	parseBrushSelected()
+	$Canvas/resize.connect("button_down",self,"_on_resize_pressed")
 
 func parseBrushSelected():
 	$Brush/TextureRect.texture = $Toolbox/HBoxContainer/Selection/HBoxContainer/Button1.group.get_pressed_button().icon
@@ -18,10 +19,12 @@ var mod_mask
 var button_mask
 func _on_background_gui_input(event):
 	
+	if not event is InputEventMouse: return
+	
 	$Brush.rect_position = event.position - $Brush.rect_size*0.5
 	
 	if event is InputEventMouseButton:
-		mod_mask = int(Input.is_key_pressed(KEY_SPACE))*32 + int(event.shift)*16 + int(event.control)*8 + int(event.alt)*4 + int(event.meta)*2 + int(event.command)
+		mod_mask = int(Input.is_key_pressed(KEY_SPACE))*32 + int(event.shift)*16 + int(event.control)*8 + int(event.alt)*4# + int(event.meta)*2 + int(event.command)*1
 		button_mask = event.button_mask
 		
 		if button_mask == BUTTON_MASK_LEFT and mod_mask == 0:
@@ -29,12 +32,15 @@ func _on_background_gui_input(event):
 	
 	
 	if event is InputEventMouseMotion:
+		if button_mask == BUTTON_MASK_MIDDLE:
+			$Canvas.rect_position += event.relative
+		
 		if button_mask == BUTTON_MASK_LEFT:
-			print(mod_mask)
+#			print(mod_mask)
 			match mod_mask:
 				32:  $Canvas.rect_position += event.relative
-				9:  $Brush.rect_size += event.relative
-				25:  $Brush.rect_rotation += event.relative.x
+				8:  $Brush.rect_size += event.relative
+				24:  $Brush.rect_rotation += event.relative.x
 				0:  paint(event.global_position)
 
 
@@ -55,7 +61,6 @@ func parseColorPicker():
 		$Toolbox/HBoxContainer/Control/HBoxContainer/ColorPickerButton2.color, \
 		$Toolbox/HBoxContainer/Control/HBoxContainer/HSlider.value)
 	
-	$Brush.color = c
 	$Brush/TextureRect.modulate = c
 
 
@@ -69,3 +74,14 @@ func _on_Button4_toggled(button_pressed):
 	parseBrushSelected()
 func _on_Button5_toggled(button_pressed):
 	parseBrushSelected()
+
+var is_canvas_resizing
+func _on_resize_pressed():
+	is_canvas_resizing = true
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if is_canvas_resizing:
+			$Canvas.rect_size += event.relative
+	else:
+		is_canvas_resizing = false
