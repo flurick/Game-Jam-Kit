7f516e18e9e48af5c9edb6577d1e37b70318a934
flurick <the3drick@gmail.com>
2020-01-28 15:58:08 +0100

    Show of one side istead of the broken -scale.x

--- HairSpray.gd
+++ HairSpray.gd
@@ -3,14 +3,15 @@ extends Line2D
 export var target = "../body"
 export var staleness = 0.3
 onready var start_points = points
-onready var start_offset = global_position - get_node(target).global_position
+onready var start_offset = get_node(target).position - position
 
 
 func _process(delta):
 	
 	
-	var scaled_offset = start_offset * get_node(target).scale
-	points[0] = get_node(target).position + scaled_offset
+#	var scaled_offset = start_offset
+#	scaled_offset.x = -get_node(target).scale.x
+	points[0] = get_node(target).position# + start_offset
 	
 	var p: Vector2
 	var last_p = points[0]
--- Polygon2D.gd
+++ Polygon2D.gd
@@ -2,10 +2,10 @@ extends Polygon2D
 
 
 func _process(delta):
-	position = get_global_mouse_position()
+	var t = OS.get_ticks_msec()*0.005
+	position = get_global_mouse_position() + Vector2(cos(t)*-10,sin(t)*10)
 
-
-func _input(event):
-	#face mouse cursor 
-	if event is InputEventMouseMotion:
-		scale.x = lerp( scale.x, clamp(-event.speed.x, -1, 1), 1 )
+#func _input(event):
+#	#face mouse cursor 
+#	if event is InputEventMouseMotion:
+#		scale.x = lerp( scale.x, clamp(-event.speed.x, -1, 1), 1 )
